94fed0dcd2803928460fec7338bedba81672bd02
Dhanush Rahul Sai <dhanushrahul555@gmail.com>
2024-09-17 10:38:39 -0300

    Backend - CRUD expenses, login and register

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_secret_key')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///expenses.db'  # Use SQLite for simplicity
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'my_jwt_secret_key')
