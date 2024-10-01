a22412f0dab9d1350121d2717ff5801b4f7bb77e
Dhanush Rahul Sai <dhanushrahul555@gmail.com>
2024-10-01 17:50:14 -0300

    Add Expense page UI updated

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axiosInstance from '../utils/axiosInstance';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const [expenses, setExpenses] = useState([]);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchExpenses = async () => {
       const token = localStorage.getItem('token');
@@ -15,10 +16,12 @@ const Dashboard = () => {
     };
     fetchExpenses();
   }, []);
-
+  const addExpense = () => {
+    navigate('/add-expense');
+}
   return (
-    <div className="min-h-screen flex justify-center items-center bg-gradient-to-b from-gray-200 to-gray-300 text-black">
-      <div className="w-full max-w-3xl mx-10 p-8 bg-white rounded-lg shadow-md">
+    <div className="min-h-screen grid grid-cols-2 gap-2 justify-center bg-gradient-to-b from-gray-200 to-gray-300 text-black">
+      <div className="mx-10 p-8 bg-white rounded-lg shadow-md relative">
         <h2 className="text-2xl font-bold text-center mb-6">Your Expenses</h2>
 
         {expenses && expenses.length > 0 ? (
@@ -45,6 +48,13 @@ const Dashboard = () => {
         ) : (
           <p className="text-center text-gray-600">No expenses available.</p>
         )}
+        <button type="button" className="absolute bottom-0 right-0 px-6 py-3 m-2 text-lg bg-teal-500 text-white rounded-lg hover:bg-green-600 transition" onClick={()=>{addExpense()}}>Add Expense</button>
+      </div>
+      <div className="mx-10 p-8 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center mb-6">Monthly Report</h2>
+      </div>
+      <div className="mx-10 p-8 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center mb-6">Categorical Report</h2>
       </div>
     </div>
   );
